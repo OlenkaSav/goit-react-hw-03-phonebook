055679313ff8c66211e848d50e91ca85fcd69cd3
OlenkaSav <94956727+OlenkaSav@users.noreply.github.com>
2022-07-31 20:18:35 +0300

    sss

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { AiOutlinePhone } from 'react-icons/ai';
-// import { nanoid } from 'nanoid';
 
 const ContactList = ({ contacts, onDeleteContact }) => (
   <ul>
